b6fdddad4cb54ebabc9abd22e3935b3508cacc07
Edwin Corea <Edwin Corea>
2017-01-15 12:05:32 -0600

    Webpack deployment in Production

--- prod/WebpackProject/server.js
+++ prod/WebpackProject/server.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const webpackMiddleware = require("webpack-dev-middleware");
-const webpack = require("webpack");
-const webpackConfig = require("./webpack.config.js");
+const path = require("path");
 
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// server routes...
+app.get("/api", (req, res) => res.send({api: "API version 0.1"}));
 
-app.listen(3050, () => console.log("Listening"));
+if (process.env.NODE_ENV !== "production"){
+    const webpackMiddleware = require("webpack-dev-middleware");
+    const webpack = require("webpack");
+    const webpackConfig = require("./webpack.config.js");    
+    app.use(webpackMiddleware(webpack(webpackConfig)));
+}
+else {
+    app.use(express.static("dist")); //make /dist freely available for user access
+    //compatibility with react router and browser history module
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "dist/index.html"));
+    });
+}
+
+app.listen(process.env.PORT || 3050, () => console.log("Listening"));
